8d3d76392d2ff2e2de0b74fff6f319f616e03620
Bomi Kim <71758542+bomyuniverse@users.noreply.github.com>
2022-11-06 20:10:16 +0900

    Add Remove func
    Add Remove func

--- ToDoListTask1/ToDoListTask1/ContentView.swift
+++ ToDoListTask1/ToDoListTask1/ContentView.swift
@@ -13,8 +13,10 @@
 // 11월 6일 Todo
 // 1. 공백이 들어갈 때를 방지할 수 있는 방어코드를 적자 (구현완)
 //  1-2 Alert: 내용을 입력해주세요.
-// 2. 수정/ 삭제 버튼을 컨텍스트 메뉴로!
-// 3. - 왜? 지금처럼 배치하는 경우(버튼) 리스트를 누르는 경우와 중복될 수 있음
+// 2. 수정 / 삭제 버튼을 컨텍스트 메뉴로!
+//  - 왜? 지금처럼 배치하는 경우(버튼) 리스트를 누르는 경우와 중복될 수 있음
+//  2-1 삭제 버튼을 우선 구현하는 것으로!
+//  2-2 수정 버튼은 나중에 구현하는 것으로!
 
 
 import SwiftUI
@@ -34,17 +36,13 @@ struct ContentView: View {
                 
                 // TextField 내용을 저장 후 리스트로 보여줍니다.
                 Button {
-//                    inputListData()
-                    //                    // 저장을 누른 후 TextField를 초기화한다.
-                    //                    inputText = ""
                     // 조건문을 통해서 텍스트가 있으면 todolist_appen가 들어가고 없으면 추가를 할 수 없게
-                    // inputText 데이터가 있는 경우
+
                     if !inputText.isEmpty {
-                        // 버튼을 눌렀을때 입력받은 inputText를 배열에 저장한다
-                        todoListData.append("\(inputText)")
-                        print("\(todoListData)")
-                        inputText = ""
+                        // inputText 데이터가 있는 경우
+                        inputListData()
                     } else {
+                        // inputText 데이터가 없는 경우
                         showingAlert = true
                     }
                 } label: {
@@ -53,60 +51,61 @@ struct ContentView: View {
                 .alert("내용을 입력해주세요!", isPresented: $showingAlert) {
                     Button("OK") {}
                 } message: {
-                    Text("짠")
+                    // 텍스트 여러 줄 보여주기!
+                    Text("""
+                        내용을 입력하지 않으면...
+                        추가해 줄 수 없어요...
+                        우엥 ㅜㅜ
+                        """)
                 }
             }
         }
         .padding(.horizontal)
         // 입력받은 내용을 보여주는 리스트
         List {
-            
-            //forEach로 뿌려준다.
-            if !todoListData.isEmpty {
-                
-                ForEach(todoListData, id: \.self) { item in
-                    HStack {
-                        // 장식용 버전(추후 구현할 체크박스)
-                        Image(systemName: "checkmark.square")
-                        
-                        Text("\(item)")
-                        
-                        Spacer()
-                        
-                        Button {
-                            // 수정 액션 넣어야 합니다.
-                        } label: {
-                            Text("수정")
-                        }
-                        
-                        Button {
-                            // 삭제 액션 넣어야 합니다.
-                        } label: {
-                            Text("삭제")
+            Section {
+                //forEach로 뿌려준다.
+                if !todoListData.isEmpty {
+                    ForEach(todoListData, id: \.self) { item in
+                        HStack {
+                            // 장식용 버전(추후 구현할 체크박스)
+                            Image(systemName: "checkmark.square")
+                            Text("\(item)")
+                            
+                            Spacer()
+                            //                        Button {
+                            //                            // 수정 액션 넣어야 합니다.
+                            //                        } label: {
+                            //                            Text("수정")
+                            //                        }
+                            //
+                            //                        Button {
+                            //                            // 삭제 액션 넣어야 합니다.
+                            //                        } label: {
+                            //                            Text("삭제")
+                            //                        }
                         }
                     }
+                    .onDelete(perform: deleteItem)
+                } else {
+                    Text("Empty")
+                        .foregroundColor(.gray)
                 }
-                
-            } else {
-                Text("Empty")
-                    .foregroundColor(.gray)
             }
         }
     }
-    
+    func inputListData() {
+        // 버튼을 눌렀을때 입력받은 inputText를 배열에 저장한다
+        todoListData.append("\(inputText)")
+        // inputText를 초기화 합니다
+        inputText = ""
+    }
+    func deleteItem(at offsets: IndexSet) {
+        // p.272 배열에 있는 아이템을 remove하는 메소드를 활용하여
+        // 화면에서만 삭제되는 기능이 아니라 데이터에서도 삭제되게 함!
+        todoListData.remove(atOffsets: offsets)
+    }
 }
-//func inputListData() {
-    
-    //        // 삼항연산자를 통해서 텍스트가 있으면 todolist_appen가 들어가고 없으면 추가를 할 수 없게
-    //        // inputText 데이터가 있는 경우
-    //        if !inputText.isEmpty {
-    //            // 버튼을 눌렀을때 입력받은 inputText를 배열에 저장한다
-    //            todoListData.append("\(inputText)")
-    //            print("\(todoListData)")
-    //            inputText = ""
-    //        } else {
-    //        }
-//}
 
 
 struct ContentView_Previews: PreviewProvider {
