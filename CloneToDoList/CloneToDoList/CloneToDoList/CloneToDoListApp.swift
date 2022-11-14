1978409fac778c73f28b08fd234d047ba7b8924e
Bomi Kim <71758542+bomyuniverse@users.noreply.github.com>
2022-11-14 21:56:49 +0900

    [Add] Add CloneToDoList files

---
+++ CloneToDoList/CloneToDoList/CloneToDoList/CloneToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneToDoListApp.swift
+//  CloneToDoList
+//
+//  Created by 김보미 on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct CloneToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
