8d8bea6e91830bb2405a3dc0747db0291d516c64
JSPark0099 <91583287+JSPark0099@users.noreply.github.com>
2022-11-14 23:11:04 +0900

    [Feat]: level1

--- CloneToDoList/CloneToDoList/CloneToDoList/ContentView.swift
+++ CloneToDoList/CloneToDoList/CloneToDoList/ContentView.swift
@@ -7,16 +7,74 @@
 
 import SwiftUI
 
+// 계란판, 붕어빵틀
+struct TodoList: Identifiable {
+    let id = UUID()
+    var content: String
+    // 각각의 리스트마다 체크박스 불리언타입으로 확인해주기
+    var isChecked: Bool
+}
+
 struct ContentView: View {
+    
+    // 인풋텍스트를 담는 변수
+    @State private var inputText: String = ""
+    // todolists를 담는 배열
+    @State private var todoLists = [TodoList]()
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            // 앱 타이틀 "To Do List"
+            Text("You Should Do It R.I.G.H.T.N.O.W!")
+                .font(.title.bold())
+            
+            // 리스트 적어 넣을 공간(TextField)
+            HStack {
+                Image(systemName: "highlighter")
+                TextField(
+                    "Input Your Task",
+                    text: $inputText,
+                    // 엔터 치면 todoLists배열에 저장 되도록 하는 제스쳐
+                    onCommit: {
+                        appendTodoLists()
+                    })
+            }
+            
+            // 투두 리스트 쪼르르 list아래 HStack을 바로 쓰면 두 개가 충돌이 일어남. 때문에 ForEach를 사용해서 하나씩 뿌려줘야한다.
+            List() {
+                ForEach(0..<todoLists.count, id: \.self){ i in
+                    HStack {
+                        // 체크 박스 버튼
+                        Button(action: {}, label: {
+                            Image(systemName: "circle")
+                        })
+                        // 투두 리스트 텍스트
+                        Text(todoLists[i].content)
+                        Spacer()
+                        // 삭제 버튼
+                        Button(action: {}, label: {
+                            Image(systemName: "trash")
+                        })
+                    }
+                }
+                
+            }
+            .listStyle(.plain)
+            
         }
         .padding()
     }
+    
+    // 텍스트필드의 입력받은 문자열을 변수 todoLists(배열)에 저장하는 함수
+    func appendTodoLists() {
+        let addList = TodoList(content: inputText, isChecked: false)
+        
+        todoLists.append(addList)
+        inputText = ""
+        
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
